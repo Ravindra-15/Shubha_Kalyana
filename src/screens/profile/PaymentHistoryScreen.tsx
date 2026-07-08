6fe4eb6f7c70015dc64f375900194f7888b3cb52
Ravindra Kumar <ravindrakumar15075@gmail.com>
2026-07-08 14:08:31 +0530

    Bottom Tab Issue Fixed

--- src/screens/profile/PaymentHistoryScreen.tsx
+++ src/screens/profile/PaymentHistoryScreen.tsx
@@ -72,7 +72,9 @@ export default function PaymentHistoryScreen({ navigation }: any) {
       </View>
 
       {loading ? (
-        <ActivityIndicator color="#D20236" style={{ marginTop: 40 }} />
+        <View style={{ flex: 1 }}>
+          <ActivityIndicator color="#D20236" style={{ marginTop: 40 }} />
+        </View>
       ) : (
         <FlatList
           data={items}
