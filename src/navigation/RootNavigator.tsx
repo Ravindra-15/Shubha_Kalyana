264062d200497817d7d2f097d9dd695a2162c3fd
Ravindra Kumar <ravindrakumar15075@gmail.com>
2026-07-11 17:00:23 +0530

    Screens Commented

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -40,14 +40,14 @@ import ChangeMpinScreen from '../screens/profile/ChangeMpinScreen';
 import PaymentHistoryScreen from '../screens/profile/PaymentHistoryScreen';
 import DeleteAccountScreen from '../screens/profile/DeleteAccountScreen';
 import ChangeEmailScreen from '../screens/profile/ChangeEmailScreen';
-import HelpSupportScreen from '../screens/profile/HelpSupportScreen';
-import FaqsScreen from '../screens/profile/FaqsScreen';
-import ContactSupportScreen from '../screens/profile/ContactSupportScreen';
-import ReportUserScreen from '../screens/profile/ReportUserScreen';
-import TermsAndConditionsScreen from '../screens/profile/TermsAndConditionsScreen';
-import PrivacyPolicyScreen from '../screens/profile/PrivacyPolicyScreen';
-import EditProfileScreen from '../screens/profile/EditProfileScreen';
-import ChooseLanguageScreen from '../screens/profile/ChooseLanguageScreen';
+// import HelpSupportScreen from '../screens/profile/HelpSupportScreen';
+// import FaqsScreen from '../screens/profile/FaqsScreen';
+// import ContactSupportScreen from '../screens/profile/ContactSupportScreen';
+// import ReportUserScreen from '../screens/profile/ReportUserScreen';
+// import TermsAndConditionsScreen from '../screens/profile/TermsAndConditionsScreen';
+// import PrivacyPolicyScreen from '../screens/profile/PrivacyPolicyScreen';
+// import EditProfileScreen from '../screens/profile/EditProfileScreen';
+// import ChooseLanguageScreen from '../screens/profile/ChooseLanguageScreen';
 import PrivacySettingsScreen from '../screens/profile/PrivacySettingsScreen';
 const Stack = createNativeStackNavigator();
 
@@ -87,14 +87,14 @@ export default function RootNavigator() {
             <Stack.Screen name="PaymentHistory" component={PaymentHistoryScreen} />
             <Stack.Screen name="DeleteAccount" component={DeleteAccountScreen} />
             <Stack.Screen name="ChangeEmail" component={ChangeEmailScreen} />
-            <Stack.Screen name="HelpSupport" component={HelpSupportScreen} />
+            {/* <Stack.Screen name="HelpSupport" component={HelpSupportScreen} />
             <Stack.Screen name="Faqs" component={FaqsScreen} />
             <Stack.Screen name="ContactSupport" component={ContactSupportScreen} />
             <Stack.Screen name="ReportUser" component={ReportUserScreen} />
             <Stack.Screen name="TermsAndConditions" component={TermsAndConditionsScreen} />
             <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicyScreen} />
             <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-            <Stack.Screen name="ChooseLanguage" component={ChooseLanguageScreen} />
+            <Stack.Screen name="ChooseLanguage" component={ChooseLanguageScreen} /> */}
             <Stack.Screen name="PrivacySettings" component={PrivacySettingsScreen} />
           </>
         ) : (
